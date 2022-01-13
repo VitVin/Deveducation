3823750da33f77fca9479bf3ec9b4bf635f826e9
VitVin <93376524+VitVin@users.noreply.github.com>
2022-01-13 19:08:31 +0300

    Add files via upload

---
+++ src/js/DOM.js
@@ -0,0 +1,7 @@
+export let DOM = {
+     countrySelector : document.getElementById('country'),
+     jobSelector : document.getElementById('jobs'),
+     typeSelector : document.getElementById('type'),
+     button : document.getElementById('apply'),
+     canvas : document.getElementById('diagram')
+}
